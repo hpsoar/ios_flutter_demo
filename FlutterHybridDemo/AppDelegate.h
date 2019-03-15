0a5282cde9033db0ff1c9e61c03ac65e150a519a
Huang Peng <huangpeng@chunyu.me>
2019-03-15 11:46:58 +0800

    Initial Commit

---
+++ FlutterHybridDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlutterHybridDemo
+//
+//  Created by Huang Peng on 2019/3/15.
+//  Copyright © 2019 Lattern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlutterHybridDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlutterHybridDemo
+//
+//  Created by Huang Peng on 2019/3/15.
+//  Copyright © 2019 Lattern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
